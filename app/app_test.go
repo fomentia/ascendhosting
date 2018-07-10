76f74c5482b7513fee8e4bd2255e4832a4bb37f1
Isaac Williams <isaac@thewilliams.ws>
2018-07-10 10:04:16 -0400

    Fill out hosts test

--- app/app_test.go
+++ app/app_test.go
@@ -1,9 +1,11 @@
 package app
 
 import (
-	"log"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/fomentia/ascendhosting/database"
@@ -15,31 +17,68 @@ type mockDB struct {
 }
 
 func (db *mockDB) Insert(model models.Model) (validationErrors models.Errors, databaseError error) {
-	log.Println("Insert called on mockDB")
-	return models.Errors{}, nil
+	validationErrors = models.Validate(model)
+
+	if validationErrors.None() {
+		row := database.Row{}
+		for index, column := range strings.Split(model.Columns(), ", ") {
+			row[column] = model.Values()[index]
+		}
+		db.Rows = append(db.Rows, row)
+	}
+
+	return
 }
 
 func (db *mockDB) Get(model models.Model) ([]database.Row, error) {
-	return []database.Row{
-		database.Row{
-			"firstName": "Peter",
-			"lastName":  "Parker",
-		},
-	}, nil
+	return db.Rows, nil
 }
 
-func TestGetHostsHandler(t *testing.T) {
-	req, reqErr := http.NewRequest("GET", "/hosts", nil)
+func TestHosts(t *testing.T) {
+	DB = &mockDB{}
+
+	requestParams := url.Values{"firstName": {"Peter"}, "lastName": {"Parker"}}
+	req, reqErr := http.NewRequest("POST", "/hosts/create", strings.NewReader(requestParams.Encode()))
 	if reqErr != nil {
 		t.Fatal(reqErr)
 	}
 
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
 	rr := httptest.NewRecorder()
-	DB = &mockDB{}
-	handler := IndexHandler(&models.Host{})
+	handler := InsertHandler(&models.Host{})
 	handler.ServeHTTP(rr, req)
 
 	if rr.Code != http.StatusOK {
 		t.Errorf("Wanted response code %v, but got %v", http.StatusOK, rr.Code)
+	}
+
+	req, reqErr = http.NewRequest("GET", "/hosts", nil)
+	if reqErr != nil {
+		t.Fatal(reqErr)
+	}
+
+	rr = httptest.NewRecorder()
+	handler = IndexHandler(&models.Host{})
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("Wanted response code %v, but got %v", http.StatusOK, rr.Code)
+	}
+
+	var data interface{}
+	decodeError := json.NewDecoder(rr.Body).Decode(&data)
+	if decodeError != nil {
+		t.Fatal(decodeError)
+	}
+
+	if data == nil {
+		t.Error("Expected data from hosts endpoint but received nothing")
+		t.FailNow()
+	}
+
+	parker := data.([]interface{})[0]
+	if parker.(map[string]interface{})["first_name"] != "Peter" {
+		t.Errorf("Expected firstName to be Peter, not %v", parker.(map[string]interface{})["firstName"])
 	}
 }
